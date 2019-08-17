0c9b1e93fd3304b0bf2f4d68e0be7f7128949cba
Logon Aluno <logonaluno@local.com>
2019-08-17 12:18:11 -0300

    Calculadora Flex

---
+++ app/src/main/java/br/com/heiderlopes/calculadoraflex/ui/login/LoginActivity.kt
@@ -0,0 +1,51 @@
+package br.com.heiderlopes.calculadoraflex.ui.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import br.com.heiderlopes.calculadoraflex.R
+import br.com.heiderlopes.calculadoraflex.ui.form.FormActivity
+import br.com.heiderlopes.calculadoraflex.ui.signup.SignUpActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private val newUserRequestCode = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+        if (mAuth.currentUser != null) {
+            goToHome()
+        }
+        btLogin.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(
+                inputLoginEmail.text.toString(),
+                inputLoginPassword.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    goToHome()
+                } else {
+                    Toast.makeText(this@LoginActivity, it.exception?.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        btSignup.setOnClickListener {
+            startActivityForResult(
+                Intent(this, SignUpActivity::class.java),
+                newUserRequestCode)
+        }
+    }
+    private fun goToHome() {
+        val intent = Intent(this, FormActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/br/com/saraiva/calculadoraflex/model/User.kt
@@ -0,0 +1,7 @@
+package br.com.saraiva.calculadoraflex.model
+
+data class User(
+    val nome: String = "",
+    val email: String = "",
+    val fone: String = ""
+)
